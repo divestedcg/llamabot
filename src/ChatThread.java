131e9aeaa105ca732ff4d01557fce5de5cd0a6aa
Tad <tad@spotco.us>
2023-05-02 16:37:37 -0400

    Missing license header
    Signed-off-by: Tad <tad@spotco.us>

--- src/ChatThread.java
+++ src/ChatThread.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2023 Divested Computing Group
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import tigase.jaxmpp.core.client.exceptions.JaxmppException;
 import tigase.jaxmpp.core.client.xmpp.modules.chat.Chat;
 import tigase.jaxmpp.core.client.xmpp.modules.chat.MessageModule;
